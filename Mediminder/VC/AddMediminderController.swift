93629b33d6c001e3175eae6e873c155f52f19a01
Kaari Strack <kaari.strack@gmail.com>
2018-03-20 17:11:23 +0000

    started adding functionality to keyboard mechanisms

---
+++ Mediminder/VC/AddMediminderController.swift
@@ -0,0 +1,64 @@
+//
+//  AddMediminderController.swift
+//  Mediminder
+//
+//  Created by Kaari Strack on 20/03/2018.
+//  Copyright © 2018 HSJK. All rights reserved.
+//
+
+import UIKit
+
+class AddMediminderController: UIViewController {
+    
+    // MARK: Outlets
+
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow(with:)),
+            name: .UIKeyboardWillShow,
+            object: nil
+        )
+    }
+    
+    // MARK: Actions
+    
+    @objc func keyboardWillShow(with notification: Notification) {
+        let key = "UIKeyboardFrameEndUserInfoKey"
+        guard let keyboardFrame = notification.userInfo?[key] as? NSValue else { return }
+
+        let keyboardHeight = keyboardFrame.cgRectValue.height
+
+        bottomConstraint.constant = keyboardHeight
+
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+
+    }
+
+    @IBAction func cancel(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+    @IBAction func done(_ sender: UIButton) {
+        
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+}
---
+++ Mediminder/VC/MediminderTableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  MediminderTableViewController.swift
+//  Mediminder
+//
+//  Created by Kaari Strack on 20/03/2018.
+//  Copyright © 2018 HSJK. All rights reserved.
+//
+
+import UIKit
+
+class MediminderTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 10
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
