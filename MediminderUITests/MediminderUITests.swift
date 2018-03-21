49a751a6ebd2a45e1754dc0daa4f5fac5e86a860
jose <jennymarin1989@hotmail.com>
2018-03-21 11:35:33 +0000

    Passing UI test for Cancel button

--- MediminderUITests/MediminderUITests.swift
+++ MediminderUITests/MediminderUITests.swift
@@ -9,28 +9,30 @@
 import XCTest
 
 class MediminderUITests: XCTestCase {
-        
+    
+    let app = XCUIApplication()
+    
     override func setUp() {
         super.setUp()
         
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app.launch()
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+       
         super.tearDown()
     }
     
     func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        app.navigationBars["Medications"].buttons["Add"].tap()
+        XCTAssert(app.staticTexts["Add a new medication!"].exists)
+    }
+    
+    func testCancel2() {
+        app.navigationBars["Medications"].buttons["Add"].tap()
+        app.buttons["Cancel"].tap()
+        XCTAssert(app.navigationBars["Medications"].exists)
     }
     
 }
