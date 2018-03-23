dd5804f230cc37a6f54d8d8bf9ed221df4b637b2
Serena Hathi <hathi.serena@gmail.com>
2018-03-23 14:43:42 +0000

    Continue UI testing and add icons

--- MediminderUITests/MediminderUITests.swift
+++ MediminderUITests/MediminderUITests.swift
@@ -37,6 +37,8 @@ class MediminderUITests: XCTestCase {
     
     func testClickTextBoxRemovePlaceHolder() {
         app.navigationBars["Mediminder!"].buttons["Add"].tap()
+        let textView = app.textViews["Enter details here..."]
+        textView.typeText("PParacetamol")
         let returnButton = app.buttons["Return"]
         returnButton.tap()
         XCTAssertFalse(app.staticTexts["Enter details here..."].exists)
@@ -65,22 +67,52 @@ class MediminderUITests: XCTestCase {
         XCTAssertFalse(app.staticTexts["Ibuprofen"].exists)
     }
 
-    func testDeletingMedication() {
-        app.navigationBars["Mediminder!"].buttons["Add"].tap()
-        let textView = app.textViews["Enter details here..."]
-        textView.typeText("CCalpol")
-        app.buttons["Done"].tap()
-        let tablesQuery = app.tables.cells
-        tablesQuery.element(boundBy: 0).swipeLeft()
-        tablesQuery.element(boundBy: 0).buttons["Delete"].tap()
-//        app.swipeLeft()
-        XCTAssert(app.staticTexts["Calpol"].exists)
+        func testDeletingMedication() {
+            app.navigationBars["Mediminder!"].buttons["Add"].tap()
+            let textView = app.textViews["Enter details here..."]
+            textView.typeText("PParacetamol")
+            app.buttons["Done"].tap()
+            let tablesQuery = app.tables
+            tablesQuery.staticTexts["Paracetamol"].swipeLeft()
+            tablesQuery.staticTexts["Paracetamol"].swipeLeft()
+            XCTAssertFalse(app.staticTexts["Paracetamol"].exists)
     }
     
+        func testMarkingMedicationComplete() {
+            app.navigationBars["Mediminder!"].buttons["Add"].tap()
+            let textView = app.textViews["Enter details here..."]
+            textView.typeText("CCalpol")
+            app.buttons["Done"].tap()
+            let tablesQuery = app.tables
+            tablesQuery.staticTexts["Calpol"].swipeRight()
+            tablesQuery.staticTexts["Calpol"].swipeRight()
+            XCTAssertFalse(app.staticTexts["Calpol"].exists)
+    }
     
+    func testEditPage() {
+        app.navigationBars["Mediminder!"].buttons["Add"].tap()
+        let textView = app.textViews["Enter details here..."]
+        textView.typeText("GGaviscon")
+        app.buttons["Done"].tap()
+        let tablesQuery = app.tables
+        tablesQuery.staticTexts["Gaviscon"].tap()
+        XCTAssert(app.staticTexts["Add a new medication!"].exists)
+    }
     
-
-    
+    func testAbilityToEditExistingMedication() {
+        app.navigationBars["Mediminder!"].buttons["Add"].tap()
+        let textView = app.textViews["Enter details here..."]
+        textView.typeText("MMorphine")
+        app.buttons["Done"].tap()
+        let tablesQuery = app.tables
+        tablesQuery.staticTexts["Morphine"].tap()
+        let textView2 = app.textViews["Morphine"]
+        textView2.tap()
+        _ = app.keys["Delete"].tap()
+//        deleteKey.press(forDuration: 4.0)
+        textView2.typeText("MMetformin")
+        XCTAssertFalse(app.staticTexts["Metformin"].exists)
+    }
     
 }
 
