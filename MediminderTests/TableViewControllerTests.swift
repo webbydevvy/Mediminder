29aece77b1f69262f40a218cbc06c5ca137e2996
Kaari Strack <kaari.strack@gmail.com>
2018-03-24 15:58:04 +0000

    new merge

---
+++ MediminderTests/TableViewControllerTests.swift
@@ -0,0 +1,21 @@
+//
+//  TableViewControllerTests.swift
+//  MediminderTests
+//
+//  Created by Kaari Strack on 22/03/2018.
+//  Copyright © 2018 HSJK. All rights reserved.
+//
+
+import XCTest
+@testable import Mediminder
+
+class TableViewControllerTests: XCTestCase {
+    
+    var table: UITableViewController!
+    
+    func testTableHasCorrectRows() {
+        
+    }
+    
+    
+}
